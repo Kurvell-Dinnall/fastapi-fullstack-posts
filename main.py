ea28851c0b3f763c3b944a291080de912dd66be7
kurvell <znac489.live.rhul.ac.uk>
2026-09-02 23:10:48 +0100

    #1 added dummy data to main.py, changed home() to be ran for get request at root and /posts, also changed home() to return html using HTMLResponse and hide HTML responses from the docs

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 Webapp = FastAPI()
 
-@Webapp.get("/")
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Ichigo Kurasaki",
+        "title": "Bleach is Awesome",
+        "content": "Bleach has amazing writing, symoblism and character development.",
+        "date_posted": "02/09/2020",
+    },
+
+    {
+        "id": 2,
+        "author": "Son Goku",
+        "title": "Dragon Ball Z is Awesome",
+        "content": "Dragon Ball Z has amazing fights, a great power system and likable characters",
+        "date_posted": "03/09/2020",
+    },
+
+]
+
+@Webapp.get("/", response_class = HTMLResponse, include_in_schema=False)
+@Webapp.get("/posts", response_class = HTMLResponse, include_in_schema=False)
 def home():
-    return {"message":"Hello World"}
+    return f"<h1>{posts[0]['title']}</h1>"
+
+@Webapp.get("/api/posts")
+def getposts():
+    return posts
